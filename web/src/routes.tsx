47d29b9b9217db401c06abedcad8d2e29ddf0508
Renata Mitsue <rmkarato@gmail.com>
2020-10-13 20:44:23 -0300

    orphanages map page: done

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
-// import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanagesMap from "./pages/OrphanagesMap";
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing} />
-                {/* <Route path="/app" component={OrphanagesMap} /> */}
+                <Route path="/app" component={OrphanagesMap} />
             </Switch>
         </BrowserRouter>
 
