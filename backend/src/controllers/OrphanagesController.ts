0cc35840141773dca1c91c8eafb759e5ca20fc0a
Renata Mitsue <rmkarato@gmail.com>
2020-10-14 11:16:50 -0300

    api get orphanage is working

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -3,6 +3,25 @@ import { getRepository } from "typeorm";
 import Orphanage from "../models/Orphanage";
 
 export default {
+  async index(req: Request, res: Response) {
+    try {
+      const orphanagesRepository = getRepository(Orphanage);
+  
+      const orphanages = await orphanagesRepository.find();
+
+      return res
+        .status(200)
+        .json(orphanages);
+
+    } catch(error) {
+      res
+        .status(error.errorCode || 400)
+        .send({
+          message: error.message
+        });
+    }
+  },
+
   async create(req: Request, res: Response) {
     try {
       const {
@@ -34,6 +53,7 @@ export default {
         .send({
           message: "Orfanato criado com sucesso!"
         });
+
     } catch(error) {
       res
         .status(error.errorCode || 400)
