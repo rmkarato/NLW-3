6a4ed5ac7d75a1cfeb43b0f027649a1a3d99e7fd
Renata Mitsue <rmkarato@gmail.com>
2020-10-13 22:29:03 -0300

    database create and connected, migrations are  working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
+import "./database/connection";
+
 const app = express();
 
 app.get("/users", (request, response) => {
-    return response.json({message: "Hello World"});
+    return response.json({ message: "Hello World" });
 });
 
 app.listen(3003);
